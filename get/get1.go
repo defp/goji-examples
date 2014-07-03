d189d6b3ab2cc3a4b0c50f664ee0ee1a8ae197fd
lidashuang <lidashuang@pinidea.com>
2014-07-03 20:01:11 +0800

    fixed get

--- get/get1.go
+++ get/get1.go
@@ -16,12 +16,12 @@ func main() {
 	// get1: url parameters
 	// visit http://127.0.0.1:8000/hello/world
 	// the name is parameter
-	// the result => get.png
+	// the result => get1_1.png
 	goji.Get("/hello/:name", hello)
 
 	// Query String Parameters
 	// for example visit: http://127.0.0.1:8000/world?name=helloword
-	// the result => get1.png
+	// the result => get1_2.png
 	goji.Get("/world", func(c web.C, w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w, "hello params is %s", r.FormValue("name"))
 	})
