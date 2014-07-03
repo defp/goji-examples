68931029e4939001e253b3ec692bce5247cf5e2d
lidashuang <lidashuang@pinidea.com>
2014-07-03 22:12:30 +0800

    gzip example

---
+++ gzip/main.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+        "fmt"
+        "net/http"
+		"compress/gzip"
+		"strings"
+
+        "github.com/zenazn/goji"
+)
+
+
+func main() {
+
+		// use gzip handle for every request
+		goji.Use(gzipHandler)
+
+        goji.Get("/", func(w http.ResponseWriter, r *http.Request){
+			fmt.Fprint(w, "helloworld..........")
+		})
+
+        goji.Serve()
+}
+
+// gzip handler
+
+type gzipResponseWriter struct {
+	*gzip.Writer
+	http.ResponseWriter
+}
+
+func (w *gzipResponseWriter) Header() http.Header {
+	return w.ResponseWriter.Header()
+}
+
+func (w *gzipResponseWriter) Write(b []byte) (int, error) {
+	h := w.ResponseWriter.Header()
+	if h.Get("Content-Type") == "" {
+		h.Set("Content-Type", http.DetectContentType(b))
+	}
+
+	return w.Writer.Write(b)
+}
+
+func gzipHandler(h http.Handler) http.Handler {
+	fn := func(w http.ResponseWriter, r *http.Request) {
+		if !strings.Contains(r.Header.Get("Accept-Encoding"), "gzip") {
+			h.ServeHTTP(w, r)
+			return
+		}
+
+		w.Header().Set("Content-Encoding", "gzip")
+		w.Header().Set("Vary", "Accept-Encoding")
+		gw := gzip.NewWriter(w)
+		defer gw.Close()
+
+		w = &gzipResponseWriter{gw, w}
+
+		h.ServeHTTP(w, r)
+	}
+	return http.HandlerFunc(fn)
+}
